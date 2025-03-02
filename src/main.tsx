2d31dcaa5766a8d3da376ec1211e3079ab05f948
anbu <anbunagarathinam30@gmail.com>
2025-03-02 17:20:41 +0530

    Initial commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import theme from "./theme";  
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+const customTheme = extendTheme(theme);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ChakraProvider theme={customTheme}>
+      <App />
+    </ChakraProvider>
+  </React.StrictMode>
+);
---
+++ src/theme.ts
@@ -0,0 +1,9 @@
+import { extendTheme } from "@chakra-ui/react";
+
+const theme = {
+    colors: {
+        primary: "#ff0000",
+    },
+};
+
+export default theme;
